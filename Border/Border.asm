b4a7c18476e4f2c7bc71296bff7b473095e268e6
TolikRogov <tolik_rogov@bk.ru>
2025-02-10 04:21:13 +0300

    frame print

--- Border/Border.asm
+++ Border/Border.asm
@@ -10,7 +10,15 @@ left_line 	 		= 00bah
 main_char 	 		= 0020h
 right_line 	 		= 00bah
 left_bottom_corner 	= 00c8h
+bottom_line 		= 00cdh
 right_bottom_corner = 00bch
+
+x_start 			= 10
+y_start 			= 5
+len 				= 60
+height 				= 10
+window_len 			= 80
+frame_color 		= 10001101b
 
 .model tiny										;set 64 Kb model
 .code											;define code block
@@ -18,41 +26,112 @@ org 100h										;prog's begging ram block
 
 Start:	mov bx, 0b800h
 		mov es, bx
-
 		mov bx, offset String
-		mov dh, 00001111b
-		mov dl, 9
+		call SetString
+		mov bx, offset String
+
+		mov dh, frame_color
+		mov dl, len
+		mov di, 0
+		add di, y_start * window_len * 2 + x_start * 2
+		call PrintString
+
+		mov cx, height - 2
+		cycle1:
+			add di, window_len * 2
+			call PrintString
+			sub bx, 3
+		loop cycle1
+		add bx, 3
+
+		add di, window_len * 2
 		call PrintString
 
 		mov ax, 4c00h							;ax = cmd(4c)
 		int 21h									;call scm
 
 ;------------------------------------------------------------------------------
-; Draws string to console (x = 0, y = 0)
-; Entry: 		BX = string address
-;				DL = string size
-;				DX = color
+; Set String
+; Entry: 		None
 ; Exit:			None
-; Destroyed:	AX, BX, CX
+; Destroyed:	None
 ;------------------------------------------------------------------------------
 
-PrintString	proc
+SetString	proc
 
-			mov di, 0
-			mov cl, dl
-
-			cycle:
-			mov al, [bx]
-			mov ah, dh
-			stosw
+			mov byte ptr ds: [bx], left_top_corner
 			inc bx
-			loop cycle
+
+			mov byte ptr ds: [bx], top_line
+			inc bx
+
+			mov byte ptr ds: [bx], right_top_corner
+			inc bx
+
+			mov byte ptr ds: [bx], left_line
+			inc bx
+
+			mov byte ptr ds: [bx], main_char
+			inc bx
+
+			mov byte ptr ds: [bx], right_line
+			inc bx
+
+			mov byte ptr ds: [bx], left_bottom_corner
+			inc bx
+
+			mov byte ptr ds: [bx], bottom_line
+			inc bx
+
+			mov byte ptr ds: [bx], right_bottom_corner
+			inc bx
 
 			ret										;return function value
 			endp									;proc's ending
 
 ;------------------------------------------------------------------------------
 
-String db '123456789'
+;------------------------------------------------------------------------------
+; Draws string to console
+; Entry: 		BX = string address
+;				DL = string size
+;				DH = color
+; Exit:			None
+; Destroyed:	AX, BX, CX
+;------------------------------------------------------------------------------
+
+PrintString	proc
+
+			mov ah, dh								;set symbols color
+
+													;Print first string symbol
+			mov al, [bx]							;al = *bx
+			stosw									;mov es: [di], ax / di += 2
+			inc bx									;bx++
+
+													;Print second string symbols
+			mov si, cx
+			mov cl, dl								;counter = dl
+			sub cl, 2
+			cycle:									;while
+				mov al, [bx]						;al = *bx
+				stosw								;mov es: [bx], ax / di += 2
+			loop cycle								;goto cycle
+			inc bx									;bx++
+			mov cx, si
+
+													;Print third string symbol
+			mov al, [bx]							;al = [bx]
+			stosw									;mov es: [bx], ax / di += 2
+			inc bx									;bx++
+
+			sub di, len * 2
+
+			ret										;return function value
+			endp									;proc's ending
+
+;------------------------------------------------------------------------------
+
+String db '<_>| |<_>'
 
 end 	Start									;pog's ending
