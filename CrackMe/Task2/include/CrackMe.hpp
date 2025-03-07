29dde41bdbb968e730075a273b03efc7c031247c
TolikRogov <tolik_rogov@bk.ru>
2025-03-07 03:43:33 +0300

    correct size of button]

--- CrackMe/Task2/include/CrackMe.hpp
+++ CrackMe/Task2/include/CrackMe.hpp
@@ -16,6 +16,10 @@ const unsigned int MODE_HEIGHT					= 358;
 const Int32 TIME_TO_UPDATE						= 100;
 const unsigned int BACKGROUND_FRAMES			= 15360;
 
+//Button properties
+const unsigned int BUTTON_WIDTH					= 313;
+const unsigned int BUTTON_HEIGHT				= 130;
+
 //Main block tittle properties
 #define _MAIN_BLOCK_TITLE_COLOR_ 255, 255, 255
 const char MAIN_BLOCK_TITLE[]					= "CrackYou";
@@ -33,3 +37,5 @@ const unsigned int MAIN_BLOCK_TITLE_Y			= 10;
 //Images segment
 #define IMAGES_PATH	DATA_PATH "img/"
 #define BACKGROUND IMAGES_PATH "background.png"
+#define BUTTON_HOVER_OFF IMAGES_PATH "button_hover_off.png"
+#define BUTTON_HOVER_ON IMAGES_PATH "button_hover_on.png"
--- CrackMe/Task2/source/main.cpp
+++ CrackMe/Task2/source/main.cpp
@@ -4,27 +4,42 @@ int main() {
 
 	srand(uint(time(NULL)));
 
+	// Create window
 	RenderWindow window = {};
 	window.create(VideoMode(MODE_WIDTH, MODE_HEIGHT), "TGF CrackYou");
 
+	// upload font
 	Font unispace = {};
 	if (!unispace.loadFromFile(UNISPACE))
 		return CRACKME_FONT_LOAD_ERROR;
 
+	// set font to main title
 	Text main_block_title = {};
 	main_block_title.setFont(unispace);
 
+	// title properties
 	main_block_title.setString(MAIN_BLOCK_TITLE);
 	main_block_title.setCharacterSize(MAIN_BLOCK_TITLE_SIZE);
 	main_block_title.setFillColor(Color(_MAIN_BLOCK_TITLE_COLOR_));
 	main_block_title.setPosition(Vector2f(MAIN_BLOCK_TITLE_X, MAIN_BLOCK_TITLE_Y));
 
+	// Background properties
 	Texture background_texture = {};
 	if (!background_texture.loadFromFile(BACKGROUND))
-		return CRACKME_ING_LOAD_ERROR;
+		return CRACKME_IMG_LOAD_ERROR;
 
 	IntRect background_rectangle({0, 0}, {MODE_WIDTH, MODE_HEIGHT});
 	Sprite background(background_texture, background_rectangle);
+
+	// Button properties
+	Texture button_texture = {};
+  	if (!button_texture.loadFromFile(BUTTON_HOVER_OFF))
+    	return CRACKME_IMG_LOAD_ERROR;
+
+	Sprite button_sprite = {};
+ 	button_sprite.setPosition(0.0f, 0.0f);
+	button_sprite.setTexture(button_texture);
+	button_sprite.setTextureRect(IntRect({0, 0}, {BUTTON_WIDTH, BUTTON_HEIGHT}));
 
 	Clock clock;
 
@@ -39,10 +54,24 @@ int main() {
 					break;
 				}
 
+				case Event::MouseMoved: {
+					Vector2i mousePos = Mouse::getPosition(window);
+					Vector2f mousePosF(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
+
+					if (button_sprite.getGlobalBounds().contains(mousePosF))
+						button_sprite.setColor(Color(250, 20, 20));
+					else
+						button_sprite.setColor(Color(255, 255, 255));
+
+					break;
+				}
+
 				default:
 					break;
 			}
         }
+
+		window.setSize({MODE_WIDTH, MODE_HEIGHT});
 
 		if (clock.getElapsedTime().asMilliseconds() > TIME_TO_UPDATE) {
 			if (background_rectangle.left + int(MODE_WIDTH) == BACKGROUND_FRAMES)
@@ -57,6 +86,7 @@ int main() {
         window.clear();
 		window.draw(background);
 		window.draw(main_block_title);
+		window.draw(button_sprite);
         window.display();
     }
 
